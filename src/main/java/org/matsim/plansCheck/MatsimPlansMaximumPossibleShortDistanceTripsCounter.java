a580ffe5699f933f7d38e23aaa7ee7a9cdba4eb9
Amit Agarwal <amit.agarwal@campus.tu-berlin.de>
2018-06-21 16:39:28 +0200

    including facilities to get the coordinate

--- src/main/java/org/matsim/plansCheck/MatsimPlansMaximumPossibleShortDistanceTripsCounter.java
+++ src/main/java/org/matsim/plansCheck/MatsimPlansMaximumPossibleShortDistanceTripsCounter.java
@@ -19,8 +19,8 @@
 
 package org.matsim.plansCheck;
 
-import org.matsim.NEMOUtils;
 import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Plan;
@@ -28,8 +28,10 @@ import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.api.core.v01.population.PopulationWriter;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.FacilitiesConfigGroup;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.facilities.ActivityFacilities;
 import org.matsim.utils.objectattributes.attributable.AttributesUtils;
 
 /**
@@ -44,12 +46,24 @@ public class MatsimPlansMaximumPossibleShortDistanceTripsCounter {
 
     public static void main(String[] args) {
 
-        String inputPlansFile = "../shared-svn/projects/nemo_mercator/data/matsim_input/2018-05-28_shorterIntraZonalDist/plans_1pct_fullChoiceSet_coordsAssigned_splitActivities_filteredForRuhr.xml.gz";
-
+//        String inputPlansFile = "../shared-svn/projects/nemo_mercator/data/matsim_input/2018-05-28_shorterIntraZonalDist/plans_1pct_fullChoiceSet_coordsAssigned_splitActivities_filteredForRuhr.xml.gz";
+        String inputPlansFile = "../runs-svn/nemo/marginals/output/testCalib/ITERS/it.0/testCalib.0.plans.xml.gz";
         String outPopulationFile = "../runs-svn/nemo/marginals/input/plans_1pct_filteredForRuhr_maxShortTrips.xml.gz";
+
+        String facilityFile = "../runs-svn/nemo/marginals/output/testCalib/testCalib.output_facilities.xml.gz";
+
         Population outPopulation = ScenarioUtils.createScenario(ConfigUtils.createConfig()).getPopulation();
 
-        Population population = NEMOUtils.loadScenarioFromPlans(inputPlansFile).getPopulation();
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        scenario.getConfig().plans().setInputFile(inputPlansFile);
+        scenario.getConfig().facilities().setInputFile(facilityFile);
+        scenario.getConfig().facilities().setFacilitiesSource(FacilitiesConfigGroup.FacilitiesSource.fromFile);
+
+        ScenarioUtils.loadScenario(scenario);
+
+        Population population = scenario.getPopulation();
+        ActivityFacilities activityFacilities = scenario.getActivityFacilities();
+
         // go through with all plans and take plan which has most number of short trips (0-1km)
         int shortTripsCounter = 0;
 
@@ -63,10 +77,14 @@ public class MatsimPlansMaximumPossibleShortDistanceTripsCounter {
                 Coord fromCoord = null;
                 for (PlanElement pe : plan.getPlanElements()){
                     if (pe instanceof Activity) {
+                        Activity activity = ((Activity)pe);
                       if (fromCoord ==null){
-                          fromCoord = ((Activity)pe).getCoord();
+                          fromCoord = activity.getCoord();
+                          if (fromCoord==null) fromCoord = activityFacilities.getFacilities().get(activity.getFacilityId()).getCoord();
                       } else{
-                          Coord toCoord = ((Activity)pe).getCoord();
+                          Coord toCoord = activity.getCoord();
+                          if (toCoord==null) toCoord = activityFacilities.getFacilities().get(activity.getFacilityId()).getCoord();
+
                           double dist = beelineDistanceFactor * NetworkUtils.getEuclideanDistance(fromCoord, toCoord);
                           if (dist > lowerLimit && dist<= upperLimit) numberOfShortTrips++;
 
