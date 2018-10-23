54656fb06d2a16bcc0cfbd2fe4f73a5a09003a81
Janek Laudan <laudan@tu-berlin.de>
2018-10-23 14:33:54 +0200

    Next try to upload build artifacts to math cluster automatically

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,7 @@ echo "Starting maven clean"
 mvn clean
 echo "Starting maven release build"
 mvn -Prelease -DskipTests=true
-echo "Setting up ssh-agent"
-eval $(ssh-agent -s)
-ssh-add <(echo "$SSH_PRIVATE_KEY")
 NOW=$(date +"%Y_%m_%d_%T")
 
 echo "Copying release file to math cluster"
-scp target/nemo-0.0.1-SNAPSHOT-release.zip $CLUSTER_USER@cluster.math.tu-berlin.de:/net/ils3/nemo_mercartor/nemo-release_$NOW.zip
+scp scp -i $SSH_PRIVATE_KEY target/nemo-0.0.1-SNAPSHOT-release.zip $CLUSTER_USER@cluster.math.tu-berlin.de:/net/ils3/nemo_mercartor/nemo-release_$NOW.zip
