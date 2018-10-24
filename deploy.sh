19c00a4761b377e8de894ea9a5cafcdb153526f4
Janek Laudan <laudan@tu-berlin.de>
2018-10-24 15:16:00 +0200

    Next try to upload things onto the math cluster

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ NOW=$(date +"%Y_%m_%d_%T")
 
 echo "preparing ssh key"
 eval $(ssh-agent -s)
-ssh-add < ${SSH_PRIVATE_KEY}
+ssh-add < (echo ${SSH_PRIVATE_KEY})
 echo "Copying release file to math cluster"
 scp target/nemo-0.0.1-SNAPSHOT-release.zip ${CLUSTER_USER}@cluster.math.tu-berlin.de:/net/ils3/nemo_mercartor/nemo-release_$NOW.zip
