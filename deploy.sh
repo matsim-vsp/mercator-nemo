12f315a8bd232741a64bc4730713feba9bcfa7da
Janek Laudan <laudan@tu-berlin.de>
2018-10-25 09:57:41 +0200

    Give up on scp and just deploy the files to s3

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,7 @@ NOW=$(date +"%Y_%m_%d_%T")
 #echo "preparing ssh key"
 #eval $(ssh-agent -s)
 #ssh-add <(echo "$SSH_PRIVATE_KEY")
-echo "Copying release file to math cluster"
-scp -i $(echo "$SSH_PRIVATE_KEY") target/nemo-0.0.1-SNAPSHOT-release.zip ${CLUSTER_USER}@cluster.math.tu-berlin.de:/net/ils3/nemo_mercartor/nemo-release_$NOW.zip
+#echo "Copying release file to math cluster"
+#scp -i $(echo "$SSH_PRIVATE_KEY") target/nemo-0.0.1-SNAPSHOT-release.zip ${CLUSTER_USER}@cluster.math.tu-berlin.de:/net/ils3/nemo_mercartor/nemo-release_$NOW.zip
+
+aws s3 sync ./target/nemo-0.0.1-SNAPSHOT-release.zip s3://mercartor-nemo-build-files --delete
