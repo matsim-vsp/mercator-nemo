36ec7861916e0244b07cced0f06e32869c83c950
Janek <j.laudan@mailbox.org>
2018-10-31 13:28:47 +0100

    initialize counts list on object creation

--- src/main/java/org/matsim/scenarioCreation/counts/CombinedCountsWriter.java
+++ src/main/java/org/matsim/scenarioCreation/counts/CombinedCountsWriter.java
@@ -4,11 +4,12 @@ import lombok.val;
 import org.matsim.counts.Counts;
 import org.matsim.counts.CountsWriter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CombinedCountsWriter<T> {
 
-    private List<Counts<T>> countsList;
+    private List<Counts<T>> countsList = new ArrayList<>();
 
     public void addCounts(Counts<T> counts) {
         this.countsList.add(counts);
@@ -18,6 +19,7 @@ public class CombinedCountsWriter<T> {
 
         val combinedCounts = new Counts<T>();
         countsList.forEach(counts -> counts.getCounts().forEach((id, count) -> {
+            // can't use map and flat map since 'getcounts' returns a treemap which doesn't implement streaming
             combinedCounts.getCounts().put(id, count);
         }));
         val writer = new CountsWriter(combinedCounts);
