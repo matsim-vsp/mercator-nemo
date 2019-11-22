be35f9c97cb3f80692c4b5b851e3dc88f0e9e373
Janek Laudan <laudan@tu-berlin.de>
2019-11-22 16:38:32 +0100

    Set up a new Smart City runner with less changes than before, since most of the troubles are solved in matsim core or the corresponding contribs

--- src/main/java/org/matsim/nemo/runners/SmartCityRunner.java
+++ src/main/java/org/matsim/nemo/runners/SmartCityRunner.java
@@ -3,16 +3,21 @@ package org.matsim.nemo.runners;
 import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import org.apache.log4j.Logger;
 import org.locationtech.jts.geom.Geometry;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.contrib.av.robotaxi.fares.drt.DrtFareModule;
 import org.matsim.contrib.av.robotaxi.fares.drt.DrtFaresConfigGroup;
+import org.matsim.contrib.bicycle.BicycleConfigGroup;
 import org.matsim.contrib.bicycle.Bicycles;
+import org.matsim.contrib.drt.routing.DrtRoute;
+import org.matsim.contrib.drt.routing.DrtRouteFactory;
 import org.matsim.contrib.drt.run.DrtConfigGroup;
 import org.matsim.contrib.drt.run.DrtConfigs;
 import org.matsim.contrib.drt.run.DrtModeModule;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
 import org.matsim.contrib.dvrp.passenger.PassengerRequestValidator;
 import org.matsim.contrib.dvrp.run.AbstractDvrpModeQSimModule;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
@@ -20,9 +25,12 @@ import org.matsim.contrib.dvrp.run.DvrpModule;
 import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.ControlerConfigGroup;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.gis.ShapeFileReader;
 import org.matsim.nemo.runners.smartCity.ServiceAreaRequestValidator;
@@ -35,6 +43,7 @@ import java.util.stream.Collectors;
 
 public class SmartCityRunner {
 
+    private static final Logger logger = Logger.getLogger(SmartCityRunner.class);
 	private static final String drtServiceAreaAttribute = "drtServiceArea";
 
 	@Parameter(names = "-inputDir", required = true)
@@ -73,12 +82,17 @@ public class SmartCityRunner {
 
 		Controler controler = createControler(scenario);
 
+        logger.info("Start SmartCity run");
+        controler.run();
+
 	}
 
 	private Controler createControler(Scenario scenario) {
 
+        logger.info("Start creating controler");
 		Controler controler = new Controler(scenario);
 
+        logger.info("add raptor pt-router");
 		controler.addOverridingModule(new SwissRailRaptorModule());
 
 		// use the (congested) car travel time for the teleported ride mode
@@ -90,7 +104,7 @@ public class SmartCityRunner {
 			}
 		});
 
-		// configure drt
+        logger.info("Start configuring drt. Add DrtModeModule, DvrpModule, DrtFareModule and activate drt-mode");
 		DrtConfigGroup drtConfigGroup = DrtConfigGroup.getSingleModeDrtConfig(scenario.getConfig());
 		controler.addOverridingModule(new DrtModeModule(drtConfigGroup));
 		controler.addOverridingModule(new DvrpModule());
@@ -99,6 +113,7 @@ public class SmartCityRunner {
 				DvrpQSimComponents.activateModes(drtConfigGroup.getMode())
 		);
 
+        logger.info("Set passengerRequestValidator");
 		controler.addOverridingQSimModule(new AbstractDvrpModeQSimModule(drtConfigGroup.getMode()) {
 			@Override
 			protected void configureQSim() {
@@ -106,26 +121,35 @@ public class SmartCityRunner {
 			}
 		});
 
-		// add bicycle
+        logger.info("Add bicycle contrib");
 		Bicycles.addAsOverridingModule(controler);
 
+        logger.info("Done configuring controler");
 		return controler;
 	}
 
 	private Scenario loadScenario(Config config) {
 
+        logger.info("start loading scenario");
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 
 		// for debugging purposes remove 90% of the population
-		if (reducedDebugScenario)
-			scenario.getPopulation().getPersons().entrySet().removeIf(person -> Math.random() > 0.1);
+        if (reducedDebugScenario) {
+            logger.info("running in reduced mode. Removing 90% of the population. Original size is: " + scenario.getPopulation().getPersons().size());
+            scenario.getPopulation().getPersons().entrySet().removeIf(person -> Math.random() > 0.1);
+            logger.info("reduced population. New size is: " + scenario.getPopulation().getPersons().size());
+        }
 
+        logger.info("creating drt service area. Start reading in ruhr shape file");
 		Collection<Geometry> serviceArea = ShapeFileReader.getAllFeatures(Paths.get(inputDir).resolve("ruhrgebiet_boundary.shp").toString()).stream()
 				.map(simpleFeature -> (Geometry) simpleFeature.getDefaultGeometry())
 				.collect(Collectors.toList());
+        logger.info("apply service area marker to all links");
 		SmartCityNetworkModification.addDrtModeAndMarkServiceArea(scenario.getNetwork(), serviceArea, DrtConfigGroup.getSingleModeDrtConfig(config).getMode(), drtServiceAreaAttribute);
+        scenario.getPopulation().getFactory().getRouteFactories().setRouteFactory(DrtRoute.class, new DrtRouteFactory());
 
 		// replace former access- egress_walk with non_network_walk
+        logger.info("replace former access_walk and egress_walk modes with non_network_mode");
 		scenario.getPopulation().getPersons().values().parallelStream()
 				.flatMap(person -> person.getPlans().stream())
 				.flatMap(plan -> plan.getPlanElements().stream())
@@ -133,26 +157,32 @@ public class SmartCityRunner {
 				.map(element -> (Leg) element)
 				.filter(leg -> leg.getMode().equals("access_walk") || leg.getMode().equals("egress_walk"))
 				.forEach(leg -> leg.setMode(TransportMode.non_network_walk));
+
+        logger.info("finished loading scenario");
 		return scenario;
 	}
 
 	private Config loadConfig(Path inputDir, Path outputDir, String runId) {
 
-		Config config = ConfigUtils.loadConfig(inputDir.resolve("config.xml").toString());
+        Config config = ConfigUtils.loadConfig(inputDir.resolve("config.xml").toString(), new DvrpConfigGroup(),
+                new MultiModeDrtConfigGroup(), new DrtFaresConfigGroup(), new BicycleConfigGroup());
 
 		config.controler().setOutputDirectory(outputDir.toString());
+        config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
 		if (runId != null) config.controler().setRunId(runId);
 
 		config.plansCalcRoute().setInsertingAccessEgressWalk(true);
 		// remove bike and ride from teleported modes
 		config.plansCalcRoute().removeModeRoutingParams(TransportMode.bike);
 		config.plansCalcRoute().removeModeRoutingParams(TransportMode.ride);
-		config.qsim().setUsingTravelTimeCheckInTeleportation(true);
+        config.controler().setRoutingAlgorithmType(ControlerConfigGroup.RoutingAlgorithmType.FastAStarLandmarks);
 
-		// add drt config groups
-		config.addModule(new DvrpConfigGroup());
-		config.addModule(new DrtConfigGroup());
-		config.addModule(new DrtFaresConfigGroup());
+		config.qsim().setUsingTravelTimeCheckInTeleportation(true);
+        config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
+        config.qsim().setNumberOfThreads(1); //drt works only single threaded
+        config.qsim().setUsePersonIdForMissingVehicleId(false);
+
+        //set up drt config
 		DrtConfigs.adjustDrtConfig(DrtConfigGroup.getSingleModeDrtConfig(config), config.planCalcScore(), config.plansCalcRoute());
 
 		final long minDuration = 600;
@@ -165,6 +195,8 @@ public class SmartCityRunner {
 		addTypicalDurations(config, "shopping", minDuration, maxDuration, difference);
 		addTypicalDurations(config, "other", minDuration, maxDuration, difference);
 
+        // fail fast if config is inconsistent
+        //config.checkConsistency();
 		return config;
 	}
 }
